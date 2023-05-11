da4dc5948aa2a6d5036f9b78ed971d60bc2b36fb
zhongsp <patrick.zhongsp@gmail.com>
2023-05-11 12:50:52 +0800

    feat: add post card

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
